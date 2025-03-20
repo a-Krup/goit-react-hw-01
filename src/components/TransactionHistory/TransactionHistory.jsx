06838ea862a8a236ec49fa2d37adf5c22bee1605
Andrii KRUPKO <jobsdetekt.ak@gmail.com>
2025-03-20 13:58:45 +0200

    import style

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./TransactionHistory.module.css";
+import "./TransactionHistory.module.css";
 
 const TransactionHistory = ({ items }) => {
   const capitalizeFirstLetter = (str) => {
